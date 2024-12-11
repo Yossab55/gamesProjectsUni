93e3e0f7aaab210120b403872b2bf491ec7a6eae
Yossab_Samuel <yossabpro5@gmail.com>
2024-12-11 08:12:53 +0200

    add top score for every game if it's exist

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
+makeTopScore();
 
+
+//* ========== function section ============ 
+//get top score and append it to span score
+function makeTopScore() {
+  let gamesNames = getNamesOfGames();
+  let topScoreSpan = document.querySelectorAll(
+    ".container .holder .box > span"
+  );
+  let size = gamesNames.length;
+  
+
+  for (let i = 0; i < size; i++) {
+    getScoreAndAppendItTo(topScoreSpan[i], gamesNames[i])
+  }
+  // return localStorageScore;
+}
+function getNamesOfGames() {
+  let gameNodeA = document.querySelectorAll(".container .holder .box a");
+  let gameNames = [];
+  for (let i = 0; i < gameNodeA.length; i++) {
+    let href = gameNodeA[i].getAttribute("href");
+    gameNames.push(href.slice(6, href.lastIndexOf("/")));
+  }
+  return gameNames;
+}
+function getScoreAndAppendItTo(span, name) {
+  let localStorageScore = window.localStorage;
+  let score = localStorageScore.getItem(name);
+  if (score !== null) {
+    span.textContent += score;
+  } else {
+    span.textContent += "not played yet";
+  }
+}
